1fdbed773cd3dc79116af6b6c142534e5230c58c
MacBook <macbook@haden.local>
2022-04-10 22:17:07 +0000

    Added .env and port to the enviroment veribales

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express();
+
+require('dotenv/config');
+
 app.use(express.static(__dirname + '/app'));
 app.use("*",function(req,res){
     res.sendFile(path.join(__dirname,"app/index.html"));
 });
-app.listen(1337, function(){
-    console.log("Server is listening on port 1337");
+
+port = process.env.PORT || 5000;
+
+app.listen(port, function(){
+    console.log(`Server is listening on port ${port}`);
 });
